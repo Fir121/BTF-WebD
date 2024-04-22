b203ec24b265c89145c8ab3b70bfa4da44c70976
Prasannah R <prassram03@gmail.com>
2024-04-22 23:45:35 +0400

    tests

--- src/components/Pages/AboutUs.js
+++ src/components/Pages/AboutUs.js
@@ -12,21 +12,22 @@ const AboutUs = () => {
         <h1>About BITS Tech Fest</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed iaculis venenatis blandit. Quisque et consectetur ligula. Cras imperdiet rutrum diam, lacinia sagittis tortor viverra quis. Mauris vitae molestie mi. Vestibulum id tincidunt libero. Suspendisse a tempus magna. Nullam mattis, leo non fermentum posuere, massa lacus lacinia turpis, non convallis odio sem a augue. Morbi sollicitudin massa et blandit imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris id vehicula felis. Morbi vulputate risus vel semper accumsan. Morbi placerat accumsan rhoncus. Mauris ac suscipit ipsum, lacinia ultricies elit.</p>
         <br/>
-        <p>Save the date- May 1 and 2, 2024</p>
+        <p>Save the date- May 1 and 2, 2024</p> <br/>
         <h2>Themes for BTF 2024</h2>
         <div className='themes-container'>
-         <div className='theme' style={{ backgroundImage: `url(${icon1})` }}>
-          
-         </div>
-         <p> Cyber Resilience – Beyond Cyber Security </p>
-         <div className='theme' style={{ backgroundImage: `url(${icon2})` }}> 
-          
-         </div>
-         <p>Sustainable Practices for Energy and Climate Change</p>
-         <div className='theme' style={{ backgroundImage: `url(${icon3})` }}>
-          
-         </div>
-         <p> Artificial intelligence as a Driving Force for the Economy & Society</p>
+     
+         <figure className='theme'>
+           <img src={icon1} />
+           <p> Cyber Resilience – Beyond Cyber Security  </p>
+         </figure>
+         <figure className='theme'>
+           <img src={icon2} />
+           <p> Sustainable Practices for Energy and Climate Change </p>
+         </figure>
+         <figure className='theme'>
+           <img src={icon3} />
+           <p> Artificial intelligence as a Driving Force for the Economy & Society </p>
+         </figure>
         </div>
         
      </div>
